9475b1d96494622562e0dad0417ea2ed87801102
Patrick English <39728632+patrick-eng@users.noreply.github.com>
2020-05-05 17:39:42 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -15,7 +15,9 @@ shinyServer(function(input, output, session) {
     
     
     
-    # Read in and adapt data
+    # Read in and adapt data #
+    
+    # Google mobility trends
     google <- read.csv("Global_Mobility_Report.csv", stringsAsFactors = FALSE) %>% 
         filter(country_region_code == "GB") %>% 
         mutate(date = as.Date(date, format = "%Y-%m-%d"),
@@ -43,13 +45,55 @@ shinyServer(function(input, output, session) {
         droplevels()
     
     
-    # update selectize with choices
+    # ONS cases
+    ons_cases <- read.csv("coronavirus-cases_latest.csv", stringsAsFactors = FALSE) %>%
+        mutate(date = as.Date(Specimen.date, format = "%Y-%m-%d"),
+               Area.name = case_when(
+                   Area.name == "England" ~ "United Kingdom",
+                   TRUE ~ Area.name
+               )
+               ) %>% 
+        rename(`Daily New Confirmed Cases` = Daily.lab.confirmed.cases,
+               `Cumulative Confirmed Cases` = Cumulative.lab.confirmed.cases)
+    
+    
+    # ONS deaths
+    ons_deaths <- read.csv("coronavirus-deaths_latest.csv") %>% 
+        mutate(date = as.Date(Reporting.date, format = "%Y-%m-%d")) %>% 
+        rename(`Daily New Deaths` = Daily.change.in.deaths,
+               `Cumulative Deaths` = Cumulative.deaths)
+    
+    ons_data <- merge(ons_cases, ons_deaths, by=c("date", "Area.name"), all=TRUE)
+    
+    # Rainfall 
+    rain <- read.delim("HadEWP_daily_qc.txt", skip=3, header=FALSE, sep="", na.strings=-99.99) %>% 
+        mutate(YearMonth = paste(V1, V2)) %>% 
+        filter(YearMonth == "2020 3" | YearMonth == "2020 4")
+    
+    rain <- c(as.numeric(rain[1,-c(1:2, 34)]), as.numeric(rain[2,-c(1:2, 34)]))
+    
+    rainfall <- seq.Date(from=as.Date("2020-03-01"), to = Sys.Date(), by=1) %>% 
+        .[1:length(rain)] %>% 
+        as.data.frame()
+    
+    rainfall$rain <- rain
+    
+    
+    
+    
+    # update selectize with choices for areas
     updateSelectizeInput(session, "area",
                          choices = unique(as.factor(google$sub_region_1)),
                          server = TRUE)
                          
+    # update selectize with choices for areas
+    updateSelectizeInput(session, "area_c",
+                         choices = unique(as.factor(ons_data$Area.name)),
+                         server = TRUE)
     
-    # Draw the plot
+    
+    
+    # Draw the lockdown plot
     output$CompliancePlot <- renderPlot({
         
         # Check to make sure there is at least some info to plot in user selection
@@ -68,33 +112,109 @@ shinyServer(function(input, output, session) {
         data <- subset(google, sub_region_1 == area)
         
         
-        if(input$national==FALSE){
+        if(input$national==FALSE & input$rainfall==FALSE){
     
             # Graph - no national overlay
             plot(data$date, data[,input$measurement], type="l",
+                 ylim = c(-90,50),
                  main = paste(input$measurement, ": ", input$area, sep=""),
                  sub = "Grey line: No change. Red line: Lockdown announced, 23rd March 2020.",
-                 xlab = "Date",
+                 xlab = "",
                  ylab = "% Change in Activity") ; abline(0,0, col="grey") ; abline(v=as.Date("2020-03-23"), col="red")
                 
-        } else {
+        } 
+        
+        if(input$national==TRUE & input$rainfall==FALSE){
             
-            # Graph - national overlay selected
+            # Graph - national overlay selected, so get data
             national <- subset(google, sub_region_1 == "United Kingdom")
             
+            # Graph - plot national
             plot(data$date, data[,input$measurement], type="l",
+                 ylim = c(-90,50),
                  main = paste(input$measurement, ": ", input$area, sep=""),
                  sub = "Grey line: No change. Red line: Lockdown announced, 23rd March 2020.",
-                 xlab = "Date",
+                 xlab = "",
                  ylab = "% Change in Activity") ; abline(0,0, col="grey") ; abline(v=as.Date("2020-03-23"), col="red")
             par(new=TRUE)
             plot(national$date, national[,input$measurement], type = "l", col="light blue",
+                 ylim = c(-90,50),
                  xlab = "",
                  ylab = "",
                  axes = F)
             
         }
+        
+        if(input$national==FALSE & input$rainfall==TRUE) {
+            
+            # Graph - plot rainfall
+            plot(data$date, data[,input$measurement], type="l",
+                 ylim = c(-90,50),
+                 main = paste(input$measurement, ": ", input$area, sep=""),
+                 sub = "Grey line: No change. Red line: Lockdown announced, 23rd March 2020. \n Precipitation figures are in millimetres.",
+                 xlab = "",
+                 ylab = "% Change in Activity") ; abline(0,0, col="grey") ; abline(v=as.Date("2020-03-23"), col="red")
+            par(new=TRUE)
+            plot(rainfall$., rainfall$rain, type = "h", col="blue",
+                 xlab = "",
+                 ylab = "",
+                 xaxt = "n",
+                 yaxt = "n")
+            axis(side=4)
+        }
+        
+        if(input$national==TRUE & input$rainfall==TRUE) {
+            
+            # Graph - national overlay selected, so get data
+            national <- subset(google, sub_region_1 == "United Kingdom")
+            
+            # Graph - plot national and rainfall
+            plot(data$date, data[,input$measurement], type="l",
+                 ylim = c(-90,50),
+                 main = paste(input$measurement, ": ", input$area, sep=""),
+                 sub = "Grey line: No change. Red line: Lockdown announced, 23rd March 2020. \n Precipitation figures are in millimetres.",
+                 xlab = "",
+                 ylab = "% Change in Activity") ; abline(0,0, col="grey") ; abline(v=as.Date("2020-03-23"), col="red")
+            par(new=TRUE)
+            plot(national$date, national[,input$measurement], type = "l", col="light blue",
+                 ylim = c(-90,50),
+                 xlab = "",
+                 ylab = "",
+                 axes = F)
+            par(new=TRUE)
+            plot(rainfall$., rainfall$rain, type = "h", col="blue",
+                 xlab = "",
+                 ylab = "",
+                 xaxt = "n",
+                 yaxt = "n")
+            axis(side=4)
+        }
 
+    })
+    
+    output$CasesPlot <- renderPlot({
+        
+        # Check to make sure there is at least some info to plot in user selection
+        shiny::validate(
+            need(
+                !all(
+                    is.na(ons_data[ons_data$Area.name==input$area_c,input$covid_i])), 
+                "Sorry, there are no data for this particular selection."
+            ) 
+        )
+             
+            if(input$covidfig==TRUE){   
+                
+                data_c <- subset(ons_data, Area.name == input$area_c)
+            
+                # Graph - plot cases/deaths
+                plot(data_c$date, data_c[,input$covid_i], type="l",
+                     main = paste(input$covid_i, ": ", input$area_c, sep=""),
+                     xlab = "Date",
+                     ylab = input$covid_i) ; abline(v=as.Date("2020-03-23"), col="red")
+            
+   
+        }
     })
 
 })
--- ui.R
+++ ui.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(shinyWidgets)
 library(shinythemes)
+library(shinysky)
 
 
 # Define UI for application
@@ -29,7 +30,10 @@ shinyUI(fluidPage(
             of different categories, relative to a pre-COVID-19 baseline 
             (median value for corresponding day of the week from Jan 3rd to 
             Feb 6th, 2020). Geographies available include cities and county areas.
-            You can overlay national-level (UK) figures onto any other area using the checkbox."),
+            You can overlay national-level (UK) figures onto any other area using the checkbox.
+            You can also overlay UK-level precipitation figures onto the plot.
+            Also available are COVID-19 cases (local authorities in England) and death rates
+            (National and sub-national)."),
     tags$br(),
     tags$br(),
     tags$br(),
@@ -38,7 +42,7 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel(
             
-            selectInput("area", "Select area:",
+            selectizeInput("area", "Search for or select area:",
                         choices = NULL, selected = NULL
                         ),
             
@@ -57,23 +61,69 @@ shinyUI(fluidPage(
                           "Include national figures?",
                           value=FALSE),
             
+            checkboxInput("rainfall", 
+                          "Include precipitation figures?",
+                          value=FALSE),
+            
+            tags$br(),
+
+            tags$p(tags$em("Compliance Index is a simple mean
+                   of all non-missing values in individual
+                   indicators.")),
+            
+            tags$br(),
+            tags$br(),
             tags$br(),
             tags$hr(),
+            tags$br(),
+            tags$br(),
+            tags$br(),
+
+            checkboxInput("covidfig", 
+                          "Show COVID-19 Figures?",
+                          value=FALSE),
             
-            tags$p("Compliance Index is a simple mean
-                   of all non-missing values in individual
-                   indicators.")
+            selectizeInput("area_c",
+                           "Search for or select area",
+                           choices = NULL,
+                           selected = NULL),
+            
+            selectInput("covid_i", "Select COVID-19 statistic:",
+                        selected = "Daily new cases",
+                        c("Daily New Confirmed Cases",
+                          "Cumulative Confirmed Cases",
+                          "Daily New Deaths",
+                          "Cumulative Deaths")
+            ),
+
+            tags$br(),
+            
+            tags$p(tags$em("ONS cases data are measured at Local Authority level or NUTS1 regions,
+                            and as such do not map perfectly onto the Google Mobiltiy data.
+                            Data on deaths are currently not available at authority level. 
+                            Cases data are not yet available in Scotland or Wales.")),
+            
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("CompliancePlot")
-        )
+            plotOutput("CompliancePlot"),
+            
+            tags$br(),
+            tags$br(),
+            
+            plotOutput("CasesPlot")
+        ),
     ),
     
     tags$br(),
     tags$hr(),
-    tags$footer("Data come from Google's Community Mobility reports: https://www.google.com/covid19/mobility/index.html?hl=en"),
+    tags$footer(paste("Lockdown compliance data come from 'Google LLC COVID-19 Community Mobility Reports' at https://www.google.com/covid19/mobility/ Accessed: ", Sys.Date(), ".", sep="")),
+    tags$br(),
+    tags$footer(paste("Figures for confirmed cases and deaths come from the Office for National statistics, at https://coronavirus.data.gov.uk/ Accessed: ", Sys.Date(), ".", sep="")),
+    tags$br(),
+    tags$footer(paste("Precipitation data come from the Met Office Hadley Centre, at https://www.metoffice.gov.uk/hadobs/hadukp/ Accessed: ", Sys.Date(), ".", sep="")),
     tags$br(),
     tags$footer("GitHub link for application code: https://github.com/patrick-eng/LockdownTrends"),
     
